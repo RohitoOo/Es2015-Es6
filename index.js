2bb7f5f258cb0d73ef0601e036da6b880f735c91
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-22 17:39:53 +0530

    Adding Functions in Literals

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 
 
-  let template = `<h1>This is JavaScript!!</h1><p> Literal Templates Work Beautifully </p>`
+let name = "Rohito"
+
+function makeUpperCase(word){
+
+return  word.toUpperCase();
+}
+
+  let template = `
+    <h1>This is JavaScript!!</h1>
+    <p> Literal Templates Work Beautifully </p>
+    <p>Adding "" + "" can get tedious and annoying</p>
+    <p>Hi! ${name}...</p>
+    <p>Using function to Upper Case --> Hi! ${makeUpperCase(name) }</p>
+
+    `
 
 
   document.getElementById('template').innerHTML = template
+
+
+//Number features Added in ES6
+
+//Hexadicmal
+console.log(0xff)
+
+// Binary ( NEW )
+console.log(0b101011)
+
+//Octol
+console.log(0o543)
+
+console.log()
